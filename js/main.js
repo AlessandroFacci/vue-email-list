d5db830e71b4e1543f67f6d55b2c04176651f3e8
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-06 16:58:23 +0200

    index and js: add 1 mail

--- js/main.js
+++ js/main.js
@@ -3,7 +3,18 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      mail: "",
     };
+  },
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => {
+        // console.log(response.data.response);
+
+        const randomMail = response.data.response;
+
+        this.mail = randomMail;
+      });
   },
 }).mount("#app");
